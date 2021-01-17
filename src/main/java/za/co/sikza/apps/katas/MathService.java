6f303526291ff66f946595746f8642d16c1538b3
Lizo <masikisiki@gmail.com>
2021-01-17 11:40:04 +0200

    add .travis.yml

---
+++ src/main/java/za/co/sikza/apps/katas/MathService.java
@@ -0,0 +1,15 @@
+package za.co.sikza.apps.katas;
+
+public class MathService {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public double divide(int a, int b) {
+        return a/b;
+    }
+}
