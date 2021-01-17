97f016dc6c4a8e71c99e1e7ad4f703f901ed952a
Lizo <masikisiki@gmail.com>
2021-01-17 10:41:28 +0200

    initaial commit

---
+++ src/main/java/za/co/sikza/apps/katas/KatasApplication.java
@@ -0,0 +1,13 @@
+package za.co.sikza.apps.katas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/sikza/apps/katas/KatasApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.sikza.apps.katas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
